f1b249193708fd192899c53f6a444c2b5550a947
J040V1T0RJG <joaovitor.jovg73@gmail.com>
2023-12-18 17:31:30 -0300

    initial setup

---
+++ src/app/(login)/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="grid max-h-screen grid-cols-loginScreen bg-green-500">
+      <p>login</p>
+      <h1>eaiaaaaaaaaa</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        app: {
+          green: {
+            100: '#50B2C0',
+            200: '#255D6A',
+            300: '#0A313C',
+          },
+
+          purple: {
+            100: '#8381D9',
+            200: '#2A2879',
+          },
+
+          gray: {
+            100: '#F8F9FC',
+            200: '#E6E8F2',
+            300: '#D1D6E4',
+            400: '#8D95AF',
+            500: '#303F73',
+            600: '#252D4A',
+            700: '#181C2A',
+            800: '#0E1116',
+          },
+        },
+      },
+      gridTemplateColumns: {
+        loginScreen: '37.5rem 1fr',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
